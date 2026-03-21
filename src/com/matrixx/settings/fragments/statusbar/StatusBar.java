c0dd4e1c52e0bf1d71eaded170956eec2009e97a
Pranav Vashi <neobuddy89@gmail.com>
2026-03-22 02:23:02 +0530

    MatrixxLab: Add ability to toggle bluetooth battery level [2/2] Signed-off-by: Pranav Vashi <neobuddy89@gmail.com> Change-Id: Id725cf7ed6c26131baf36e826928d907ef6f46d6 Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

--- src/com/matrixx/settings/fragments/statusbar/StatusBar.java
+++ src/com/matrixx/settings/fragments/statusbar/StatusBar.java
@@ -1,11 +1,11 @@
 /*
- * Copyright (C) 2023 The LeafOS Project
+ * Copyright (C) 2022-2026 Project Matrixx
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,61 +13,68 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.matrixx.settings.fragments.statusbar;
 
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.res.Resources;
 import android.os.Bundle;
-import android.provider.Settings;
 
 import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceScreen;
 
-import com.android.internal.logging.nano.MetricsProto;
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.settings.R;
-import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settings.SettingsPreferenceFragment;
 import com.android.settingslib.search.SearchIndexable;
 
 import java.util.List;
 
-@SearchIndexable
-public class StatusBar extends SettingsPreferenceFragment
-        implements Preference.OnPreferenceChangeListener {
+import com.matrixx.settings.preferences.SystemSettingSwitchPreference;
+import com.matrixx.settings.utils.DeviceUtils;
 
-    public static final String TAG = "StatusBar";
+@SearchIndexable
+public class StatusBar extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String TAG = "StatusBar";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         addPreferencesFromResource(R.xml.matrixx_settings_statusbar);
+
+        final Context context = getContext();
+        final ContentResolver resolver = context.getContentResolver();
+        final PreferenceScreen prefScreen = getPreferenceScreen();
+        final Resources resources = context.getResources();
+
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        final Context context = getContext();
+        final ContentResolver resolver = context.getContentResolver();
         return false;
-    }
-
-    public static void reset(Context context) {
-        ContentResolver resolver = context.getContentResolver();
-
     }
 
     @Override
     public int getMetricsCategory() {
-        return MetricsProto.MetricsEvent.MATRIXX;
+        return MetricsEvent.MATRIXX;
     }
 
-    /**
-     * For search
-     */
     public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
-            new BaseSearchIndexProvider(R.xml.matrixx_settings_statusbar) {
+        new BaseSearchIndexProvider(R.xml.matrixx_settings_statusbar) {
 
-                @Override
-                public List<String> getNonIndexableKeys(Context context) {
-                    return super.getNonIndexableKeys(context);
-                }
-            };
+            @Override
+            public List<String> getNonIndexableKeys(Context context) {
+                List<String> keys = super.getNonIndexableKeys(context);
+                final Resources resources = context.getResources();
+
+                return keys;
+            }
+        };
 }
