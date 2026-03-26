8a2dfac7037a9ab7f2e4f6239903b4b272ed55a1
Ghosuto <clash.raja10@gmail.com>
2026-03-26 20:18:37 +0530

    MatrixxLab: add some wifi icons style
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

--- src/com/matrixx/settings/fragments/ui/UserInterface.java
+++ src/com/matrixx/settings/fragments/ui/UserInterface.java
@@ -1,11 +1,11 @@
 /*
- * Copyright (C) 2023 The LeafOS Project
+ * Copyright (C) 2022-2026 Project Matrixx
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,12 +13,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.matrixx.settings.fragments.ui;
-
 import android.content.ContentResolver;
 import android.content.Context;
 import android.os.Bundle;
+import android.os.UserHandle;
 import android.provider.Settings;
 
 import androidx.preference.Preference;
@@ -29,6 +28,10 @@ import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.SearchIndexable;
 
+import com.matrixx.settings.preferences.SystemSettingListPreference;
+import com.android.internal.util.matrixx.ThemeUtils;
+import com.matrixx.settings.utils.SystemRestartUtils;
+
 import java.util.List;
 
 @SearchIndexable
@@ -37,15 +40,72 @@ public class UserInterface extends SettingsPreferenceFragment
 
     public static final String TAG = "UserInterface";
 
+    private static final String KEY_WIFI_ICON_STYLE = "wifi_icon_style";
+
+    private SystemSettingListPreference mWifiIconStyle;
+    private ThemeUtils mThemeUtils;
+
+    private static final String[] WIFI_ICON_OVERLAYS = {
+            "com.custom.overlay.systemui.wifiAurora",
+            "com.android.systemui.wifibar_c",
+            "com.custom.overlay.systemui.wifiLinear",
+            "com.android.systemui.wifiNothingDot",
+            "com.android.systemui.wifibar_d"
+    };
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.matrixx_settings_ui);
+
+        mThemeUtils = ThemeUtils.getInstance(getActivity());
+
+        mWifiIconStyle = findPreference(KEY_WIFI_ICON_STYLE);
+        if (mWifiIconStyle != null) {
+            mWifiIconStyle.setOnPreferenceChangeListener(this);
+        }
+    }
+
+    private void updateStyle(String key, String category, String target,
+            int defaultValue, String[] overlayPackages, boolean restartSystemUI) {
+        final int style = Settings.System.getIntForUser(
+                getContext().getContentResolver(),
+                key,
+                defaultValue,
+                UserHandle.USER_CURRENT
+        );
+        if (mThemeUtils == null) {
+            mThemeUtils = ThemeUtils.getInstance(getContext());
+        }
+        mThemeUtils.setOverlayEnabled(category, target, target);
+        if (style > 0 && style <= overlayPackages.length) {
+            mThemeUtils.setOverlayEnabled(category, overlayPackages[style - 1], target);
+        }
+        if (restartSystemUI) {
+            SystemRestartUtils.restartSystemUI(getContext());
+        }
+    }
+
+    private void updateWifiIconStyle() {
+        updateStyle(KEY_WIFI_ICON_STYLE, "android.theme.customization.wifi_icon",
+                "com.android.systemui", 0, WIFI_ICON_OVERLAYS, true);
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        final Context context = getContext();
+        final ContentResolver resolver = context.getContentResolver();
+        int value = 0;
+
+        if (preference == mWifiIconStyle) {
+            value = Integer.parseInt((String) newValue);
+            Settings.System.putIntForUser(resolver,
+                    KEY_WIFI_ICON_STYLE, value, UserHandle.USER_CURRENT);
+            updateWifiIconStyle();
+            return true;
+        }
+
         return false;
     }
 
